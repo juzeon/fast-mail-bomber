de53fdf29abf64432764a90def1f13862b652827
juzeon <812312770@qq.com>
2021-02-12 22:34:16 +0800

    add UA to guzzle base options

--- init.php
+++ init.php
@@ -9,7 +9,10 @@ use GuzzleHttp\Client;
 $guzzle=new Client([
     'proxy'=>PROXY,
     'timeout'=>TIMEOUT,
-    'verify'=>false
+    'verify'=>false,
+    'headers'=>[
+        'User-Agent'=>'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
+    ]
 ]);
 define('HELP_TEXT',<<<EOF
 Usage:
